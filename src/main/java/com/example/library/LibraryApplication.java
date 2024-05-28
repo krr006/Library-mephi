4c6c3a8fc39621c0240380ce86958eb799d8c775
user <desper1609@gmail.com>
2024-05-28 19:39:59 +0300

    fix run-file

--- src/main/java/com/example/library/LibraryApplication.java
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -1,12 +1,6 @@
 package com.example.library;
-
-import com.example.library.entity.Book;
-import com.example.library.service.BookService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Locale;
-import java.util.Optional;
 
 @SpringBootApplication
 public class LibraryApplication {
